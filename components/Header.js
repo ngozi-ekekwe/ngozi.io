f061d2243788a77ba265f3be0940c51b0a6602ff
Ngozi Ekekwe <ngozi-ekekwe>
2020-10-20 02:24:54 +0200

    add theme ui and navigation

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import cx from "classnames";
 import styles from "../styles/Header.module.css";
 
 export default function Header() {
+
+  const [ menuOpen, setMenuOpen ] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen) 
+  };
+
+  const menuClassname = cx(styles.menu__toggle, {
+    [styles.is_active]: menuOpen,
+  });
+
+  const dropdownMenuClassname = cx(styles.nav__dropdown,{
+    [styles.nav__open] : menuOpen
+  })
+
   return (
-    <header className={styles.header}>
-      <div className={styles.header_content}>
-        <nav className={styles.nav}>
-          <a href="/" className={styles.header_logo}>
-            <span className={styles.header_logo__colored}>Ngozi.io</span>
-          </a>
-        </nav>
-      </div>
+    <header className={styles.header} role="banner" id="">
+      <a
+        id="logo"
+        href=""
+        aria-label="Logo, go to homepage"
+        className={styles.header_logo}
+      >
+        <span className={styles.header_logo__colored}>ngozi.io</span>
+      </a>
+      <button
+        type="button"
+        aria-label={menuOpen ? "close menu" : 'open menu'}
+        aria-controls="menu"
+        aria-expanded={menuOpen}
+        className={menuClassname}
+        onClick={toggleMenu}
+      >
+        <span className={styles.menu__line}></span>
+        <span className={styles.menu__line}></span>
+        <span className={styles.menu__line}></span>
+      </button>
+      <nav id="menu" aria-label="" className={dropdownMenuClassname}>
+        <ul>
+          <li className={styles.menu__item}><a href="">Work</a></li>
+          <li className={styles.menu__item}><a href="">Articles</a></li>
+          <li className={styles.menu__item}><a href="">Resume</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
---
+++ utils/theme.js
@@ -0,0 +1,16 @@
+export const colors = {
+  pink: '#EE595D',
+  pinkAlt: '#00dcdd',
+  darkPink: '#ee59a8',
+  black: '#000',
+  white: '#fff',
+  background: '#fff'
+};
+
+export const theme = {
+  colors,
+  fontFamily: `"SF UI Text", "Helvetica Neue", "Segoe UI", "Oxygen",
+  "Ubuntu", "Cantarell", "Open Sans", sans-serif`,
+}
+
+export default theme;
